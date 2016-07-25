aeb17f359bc5d43b80a3871b019d4cd47bd2e27f
Yajiex Github <yajiex@outlook.com>
2016-07-25 20:15:25 +0800

    Number-of-1-Bits

---
+++ solutions/Number-of-1-Bits/Number-of-1-Bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let ans = 0;
+  while (n !== 0) {
+    n &= (n - 1);
+    ans++;
+  }
+  return ans;
+};
