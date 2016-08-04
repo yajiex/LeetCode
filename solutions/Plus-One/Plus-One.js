b6c79de74f733e18d66e617657dde7f7dfa40bf1
Yajiex Github <yajiex@outlook.com>
2016-08-04 19:38:27 +0800

    Plus-One

---
+++ solutions/Plus-One/Plus-One.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  const n = digits.length;
+  for (let i = n - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
--- solutions/Plus-One/Plus-One.md
+++ solutions/Plus-One/Plus-One.md
@@ -10,3 +10,8 @@
 ## 更新：
 总结leetcode数学题目
 
+## Description:
+
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
