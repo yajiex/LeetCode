68fc8938d7e8ca18ed83554bd495c5db964a7056
Yajiex Github <yajiex@outlook.com>
2016-12-26 09:00:38 +0800

    Integer-to-Roman

---
+++ solutions/Integer-to-Roman/Integer-to-Roman.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    const M = ["", "M", "MM", "MMM"];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
+};
--- solutions/Integer-to-Roman/Integer-to-Roman.md
+++ solutions/Integer-to-Roman/Integer-to-Roman.md
@@ -10,3 +10,11 @@
 ## 更新：
 总结leetcode数学题目
 
+## Description:
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+## Idea:
+look up table.
+
