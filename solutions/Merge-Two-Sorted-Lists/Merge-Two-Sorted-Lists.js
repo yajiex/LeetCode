2fc0011958cb05c8d0ba11e07ff6121097939efa
Yajiex Github <yajiex@outlook.com>
2017-01-04 08:42:40 +0800

    Merge-Two-Sorted-Lists

---
+++ solutions/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  const head = new ListNode(0);
+  let p = head;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      p.next = l1;
+      l1 = l1.next;
+    } else {
+      p.next = l2;
+      l2 = l2.next;
+    }
+    p = p.next;
+  }
+  p.next = l1 ? l1 : l2;
+  return head.next;
+};
--- solutions/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.md
+++ solutions/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.md
@@ -9,3 +9,5 @@
 ## 更新：
 总结leetcode链表题目
 
+## Description:
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
