f442092c06264777911a6b79554df39f9c3e0160
Yajiex Github <yajiex@outlook.com>
2017-01-18 08:31:05 +0800

    Search-in-Rotated-Sorted-Array

---
+++ solutions/Search-in-Rotated-Sorted-Array/Search-in-Rotated-Sorted-Array.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  const n = nums.length;
+  let lo = 0, hi = n - 1;
+  // find the index of the smallest value using binary search.
+  // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+  // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+  while (lo < hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (nums[mid] > nums[hi]) {
+      lo = mid + 1;
+    } else {
+      hi = mid;
+    }
+  }
+  // lo==hi is the index of the smallest value and also the number of places rotated.
+  let rot = lo;
+  lo = 0;
+  hi = n - 1;
+  // The usual binary search and accounting for rotation.
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    const realmid = (mid + rot) % n;
+    if (nums[realmid] == target) {
+      return realmid;
+    }
+    if (nums[realmid] < target) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+  return -1;
+};
