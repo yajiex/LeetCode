08c5634c1b81ee4b4ae36ac5e11beebe221c60e8
Yajie Xue <yajiex@outlook.com>
2019-01-05 16:50:05 +0800

    Find-the-Duplicate-Number

---
+++ solutions/Find-the-Duplicate-Number/Find-the-Duplicate-Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if(nums.size() <= 1)
+        {
+            return 0;
+        }
+        
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        fast = 0;
+        while(slow != fast)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+};
+
