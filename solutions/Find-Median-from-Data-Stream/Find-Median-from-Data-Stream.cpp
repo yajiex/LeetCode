f43333adcb867686532ee472721dd970f5469e8b
yajiex@outlook.com <yajiex@outlook.com>
2019-01-26 15:32:55 +0800

    Find-Median-from-Data-Stream

---
+++ solutions/Find-Median-from-Data-Stream/Find-Median-from-Data-Stream.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/find-median-from-data-stream/discuss/74062/Short-simple-JavaC%2B%2BPython-O(log-n)-%2B-O(1)
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if (small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        return small.size() > large.size()
+               ? small.top()
+               : (small.top() - large.top()) / 2.0;
+    }
+
+private:
+    priority_queue<long> small, large;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
