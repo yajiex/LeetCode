a19aed3e2f162a02c2611353c09655885403bde5
Yajie Xue <yajiex@outlook.com>
2019-03-01 19:53:16 +0800

    Array-Partition-I

---
+++ solutions/Array-Partition-I/Array-Partition-I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        vector<int> hashtable(20001,0);
+        for(size_t i=0;i<nums.size();i++)
+        {
+            hashtable[nums[i]+10000]++;
+        }
+        int ret=0;
+        int flag=0;
+        for(size_t i=0;i<20001;){
+            if((hashtable[i]>0)&&(flag==0)){
+                ret=ret+i-10000;
+                flag=1;
+                hashtable[i]--;
+            }else if((hashtable[i]>0)&&(flag==1)){
+                hashtable[i]--;
+                flag=0;
+            }else i++;
+        }
+        return ret;
+    }
+};
