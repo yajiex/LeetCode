d9d02c6de08b51d15dd7196b788b7bebf03a12db
yajiex <yajiex@outlook.com>
2019-04-08 19:46:40 +0800

    Two-Sum-IV-Input-is-a-BST

---
+++ solutions/Two-Sum-IV-Input-is-a-BST/Two-Sum-IV-Input-is-a-BST.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> set;
+        return dfs(root, set, k);
+    }
+    
+    bool dfs(TreeNode* root, unordered_set<int>& set, int k){
+        if(root == NULL)return false;
+        if(set.count(k - root->val))return true;
+        set.insert(root->val);
+        return dfs(root->left, set, k) || dfs(root->right, set, k);
+    }
+};
