eac05582a8853256af05cba32d50c8e53768cabf
yajiex <yajiex@outlook.com>
2019-05-16 19:54:03 +0800

    Linked-List-Random-Node

---
+++ solutions/Linked-List-Random-Node/Linked-List-Random-Node.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* head;
+public:
+    /** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    /*
+    /*
+  S has items to sample, R will contain the result
+ReservoirSample(S[1..n], R[1..k])
+  // fill the reservoir array
+  for i = 1 to k
+      R[i] := S[i]
+
+  // replace elements with gradually decreasing probability
+  for i = k+1 to n
+    j := random(1, i)   // important: inclusive range
+    if j <= k
+        R[j] := S[i]
+        */
+    int getRandom() {
+        int res = head->val;
+        ListNode* node = head->next;
+        int i = 2;
+        while(node){
+            int j = rand()%i;
+            if(j==0)
+                res = node->val;
+            i++;
+            node = node->next;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
