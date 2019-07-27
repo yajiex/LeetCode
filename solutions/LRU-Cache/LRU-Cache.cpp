8e2e6329b2b23c3b0f180fb3232c12c720c73f13
Yajie Xue <yajiex@outlook.com>
2019-07-27 17:33:37 +0800

    Review - 20190727

--- solutions/LRU-Cache/LRU-Cache.cpp
+++ solutions/LRU-Cache/LRU-Cache.cpp
@@ -1,74 +1,174 @@
 
-struct node{
-    node* pre;
+// struct node{
+//     node* pre;
+//     int key;
+//     int value;
+//     node* next;
+//     node(int k, int v):key(k),value(v),pre(NULL),next(NULL){};
+// };
+
+// class LRUCache{
+//     unordered_map<int, node*> mp;
+//     int capacity;
+//     int size;
+//     node* head;
+//     node* tail;
+// public:
+//     LRUCache(int c){
+//         if(c<0)return;
+//         head = new node(-1,-1);
+//         tail = new node(-1,-1);
+//         head->next = tail;
+//         tail->pre = head;
+//         mp.clear();
+//         capacity = c;
+//         size = 0;
+//     }
+        
+//     int get(int k) {
+//         unordered_map<int, node*>::iterator it = mp.find(k);
+//         if(it != mp.end()){
+//             node* p = it->second;
+//             p->pre->next = p->next;
+//             p->next->pre = p->pre;
+//             putToHead(p);
+//             return p->value;
+//         }
+//         else
+//             return -1;
+//     }
+        
+//     void set(int k, int val) {
+//         if(capacity < 1) return; 
+//         unordered_map<int, node*>::iterator it = mp.find(k);
+//         if(it != mp.end()){
+//             node* p = it->second;
+//             p->pre->next = p->next;
+//             p->next->pre = p->pre;
+//             putToHead(p);
+//             p->value = val;
+//         }else{
+//             node* p = new node(k, val);
+//             putToHead(p);
+//             mp[k] = p;
+//             size++;
+//             if(size>capacity){
+//                 p = tail->pre;
+//                 tail->pre = p->pre;
+//                 p->pre->next = tail;
+//                 it = mp.find(p->key);
+//                 mp.erase(it);
+//                 delete p;
+//             }
+//         }
+//     }
+
+//     void putToHead(node* p)
+//     {
+//         p->next = head->next;
+//         p->pre = head;
+//         head->next->pre = p;
+//         head->next = p;
+//     }
+// };
+
+// 2019-07-27
+// O(1)
+struct Node {
+    Node* pre;
+    Node* next;
     int key;
-    int value;
-    node* next;
-    node(int k, int v):key(k),value(v),pre(NULL),next(NULL){};
+    int val;
+    Node(int k, int v)
+    {
+        key = k;
+        val = v;
+        pre = NULL;
+        next = NULL;
+    };
 };
 
-class LRUCache{
-    unordered_map<int, node*> mp;
-    int capacity;
-    int size;
-    node* head;
-    node* tail;
+class LRUCache {
 public:
-    LRUCache(int c){
-        if(c<0)return;
-        head = new node(-1,-1);
-        tail = new node(-1,-1);
+    LRUCache(int capacity) {
+        if (capacity < 0)
+        {
+            return;
+        }
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
         head->next = tail;
         tail->pre = head;
         mp.clear();
-        capacity = c;
+        maxSize = capacity;
         size = 0;
     }
-        
-    int get(int k) {
-        unordered_map<int, node*>::iterator it = mp.find(k);
-        if(it != mp.end()){
-            node* p = it->second;
+    
+    int get(int key) {
+        unordered_map<int, Node*>::iterator it = mp.find(key);
+        if(it != mp.end())
+        {
+            Node* p = it->second;
             p->pre->next = p->next;
             p->next->pre = p->pre;
             putToHead(p);
-            return p->value;
+            return p->val;
         }
         else
+        {
             return -1;
+        }
     }
-        
-    void set(int k, int val) {
-        if(capacity < 1) return; 
-        unordered_map<int, node*>::iterator it = mp.find(k);
-        if(it != mp.end()){
-            node* p = it->second;
-            p->pre->next = p->next;
-            p->next->pre = p->pre;
+    
+    void put(int key, int value) {
+        if(maxSize < 1)
+        {
+            return;
+        }
+        unordered_map<int, Node*>::iterator it = mp.find(key);
+        if(it == mp.end())
+        {
+            Node* p = new Node(key, value);
             putToHead(p);
-            p->value = val;
-        }else{
-            node* p = new node(k, val);
-            putToHead(p);
-            mp[k] = p;
+            mp[key] = p;
             size++;
-            if(size>capacity){
+            if(size > maxSize)
+            {
                 p = tail->pre;
                 tail->pre = p->pre;
                 p->pre->next = tail;
                 it = mp.find(p->key);
-                mp.erase(it);
-                delete p;
+                mp.erase(p->key);
+                size--;
             }
         }
+        else
+        {
+            Node* p = it->second;
+            p->pre->next = p->next;
+            p->next->pre = p->pre;
+            putToHead(p);
+            p->val = value;
+        }
     }
-
-    void putToHead(node* p)
-    {
+private:
+    void putToHead(Node* p) {
         p->next = head->next;
         p->pre = head;
         head->next->pre = p;
         head->next = p;
     }
+    
+    unordered_map<int, Node*> mp;
+    Node* head;
+    Node* tail;
+    int maxSize = 0;
+    int size;
 };
- 
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
--- solutions/Linked-List-Cycle-II/Linked-List-Cycle-II.cpp
+++ solutions/Linked-List-Cycle-II/Linked-List-Cycle-II.cpp
@@ -1,4 +1,44 @@
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         if(!head || !head->next)
+//             return NULL;
+//         ListNode *slow=head;
+//         ListNode *fast=head;
+//         while(slow)
+//         {
+//             slow=slow->next;
+//             if(!fast)
+//                 return NULL;
+//             fast=fast->next;
+//             if(!fast)
+//                 return NULL;
+//             fast=fast->next;
+//             if(slow==fast)
+//                 break;
+//         }
+//         if(!slow)
+//             return NULL;
+//         while(head!=slow)
+//         {
+//             head=head->next;
+//             slow=slow->next;
+//         }
+//         return head;
+//     }
+// };
+
+// 2019-07-27
+// O(n)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,32 +50,31 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head || !head->next)
-            return NULL;
-        ListNode *slow=head;
-        ListNode *fast=head;
-        while(slow)
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast)
         {
-            slow=slow->next;
-            if(!fast)
+            fast = fast->next;
+            if(fast)
+            {
+                fast = fast->next;
+            }
+            else
+            {
                 return NULL;
-            fast=fast->next;
-            if(!fast)
-                return NULL;
-            fast=fast->next;
-            if(slow==fast)
-                break;
+            }
+            slow = slow->next;
+            if(fast == slow)
+            {
+                while(head != slow)
+                {
+                    head = head->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
         }
-        if(!slow)
-            return NULL;
-        while(head!=slow)
-        {
-            head=head->next;
-            slow=slow->next;
-        }
-        return head;
+        return NULL;
     }
 };
-
-
  
