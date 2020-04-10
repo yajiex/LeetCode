9559df30184995afd530acfc6fa1922770e22fc2
Yajie Xue <yajiex@outlook.com>
2020-04-10 20:31:39 +0800

    Min-Stack

--- solutions/Min-Stack/Min-Stack.cpp
+++ solutions/Min-Stack/Min-Stack.cpp
@@ -27,28 +27,72 @@
 
 // 2019-07-28
 // O(1)
+// class MinStack {
+// public:
+//     /** initialize your data structure here. */
+//     MinStack() {
+//         st = stack<int>();
+//         minSt = stack<int>();
+//     }
+    
+//     void push(int x) {
+//         st.push(x);
+//         if(minSt.empty() || minSt.top() >= x)
+//         {
+//             minSt.push(x);
+//         }
+//     }
+    
+//     void pop() {
+//         if(st.top() == minSt.top())
+//         {
+//             minSt.pop();
+//         }
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return minSt.top();
+//     }
+// private:
+//     stack<int> st;
+//     stack<int> minSt;
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(x);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+// 2020-04-10
 class MinStack {
 public:
     /** initialize your data structure here. */
     MinStack() {
         st = stack<int>();
-        minSt = stack<int>();
+        minStack = stack<int>();
     }
     
     void push(int x) {
         st.push(x);
-        if(minSt.empty() || minSt.top() >= x)
-        {
-            minSt.push(x);
+        if(minStack.empty()) {
+            minStack.push(x);
+        } else {
+            minStack.push(min(x, minStack.top()));
         }
     }
     
     void pop() {
-        if(st.top() == minSt.top())
-        {
-            minSt.pop();
-        }
         st.pop();
+        minStack.pop();
     }
     
     int top() {
@@ -56,11 +100,12 @@ public:
     }
     
     int getMin() {
-        return minSt.top();
+        return minStack.top();
     }
+
 private:
     stack<int> st;
-    stack<int> minSt;
+    stack<int> minStack;
 };
 
 /**
@@ -71,4 +116,3 @@ private:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
- 
