a8e8e9fa2b53959658d2c6d9fd3116cc588aabf1
Yajie Xue <yajiex@outlook.com>
2020-04-18 18:55:02 +0800

    Minimum-Path-Sum

--- solutions/Minimum-Path-Sum/Minimum-Path-Sum.cpp
+++ solutions/Minimum-Path-Sum/Minimum-Path-Sum.cpp
@@ -19,6 +19,33 @@
 
 // 2019-07-21
 // O(nm)
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         if(n==0)
+//         {
+//             return 0;
+//         }
+//         int m = grid[0].size();
+//         if(m==0)
+//         {
+//             return 0;
+//         }
+//         vector<vector<int>> dp = vector<vector<int>>(n+1, vector<int>(m+1, INT_MAX));
+        
+//         for(int i=1;i<=n;i++)
+//         {
+//             for(int j=1;j<=m;j++)
+//             {
+//                 dp[i][j] = ((i==1 && j==1) ? 0 : min(dp[i-1][j], dp[i][j-1])) + grid[i-1][j-1];
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// };
+
+// 2020-04-18
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
@@ -44,4 +71,3 @@ public:
         return dp[n][m];
     }
 };
- 
