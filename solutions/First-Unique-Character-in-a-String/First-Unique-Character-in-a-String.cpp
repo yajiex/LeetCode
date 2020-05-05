177a8dcffef0eed6309786e0a968074b6487cead
Yajie Xue <yajiex@outlook.com>
2020-05-05 17:47:57 +0800

    First-Unique-Character-in-a-String

--- solutions/First-Unique-Character-in-a-String/First-Unique-Character-in-a-String.cpp
+++ solutions/First-Unique-Character-in-a-String/First-Unique-Character-in-a-String.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         vector<int> ans(26, 0);
+//         int n = s.length();
+//         for(int i=0;i<n;i++)
+//         {
+//             ans[s[i]-'a']++;
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             if(ans[s[i]-'a']==1)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// 2020-05-05
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> ans(26, 0);
-        int n = s.length();
-        for(int i=0;i<n;i++)
-        {
-            ans[s[i]-'a']++;
+        vector<int> visited(26, 0);
+        for(int i=0;i<s.length();i++) {
+            visited[s[i] - 'a']++;
         }
-        for(int i=0;i<n;i++)
-        {
-            if(ans[s[i]-'a']==1)
-            {
+        for(int i=0;i<s.length();i++) {
+            if(visited[s[i] - 'a'] == 1) {
                 return i;
             }
         }
